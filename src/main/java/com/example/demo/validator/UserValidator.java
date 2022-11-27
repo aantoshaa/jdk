1487ebbb17dbaadac4bbb8112228c11202a576c2
aantoshaa_ <aapalanjuk@gmail.com>
2022-11-27 14:02:57 +0300

    Add user validation via login

---
+++ src/main/java/com/example/demo/validator/UserValidator.java
@@ -0,0 +1,37 @@
+package com.example.demo.validator;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class UserValidator implements Validator {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    public UserValidator(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return User.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+          User user = (User) target;
+
+          try {
+              userDetailsService.loadUserByUsername(user.getLogin());
+          } catch (UsernameNotFoundException ignored) {
+              return;
+          }
+            errors.rejectValue("login", "", "user.login.exists");
+    }
+}
